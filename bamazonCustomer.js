340bdedda20b9aac454d97d4d98786b1c83e3f1c
Ilamaran Gurunathan <ilamaran.g@gmail.com>
2017-06-19 22:53:27 -0400

    Added functionality to display Receipt

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,87 +3,115 @@ var Table = require('cli-table');
 var inquirer = require('inquirer');
 var questions = require('./questions.js');
 var query = require('./query.js');
-var tableDisplayDefinition = require('./tableHeaders.js')
+var tableDisplayDefinition = require('./tableHeaders.js');
+var colors = require('colors/safe');
 
 var connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: '',
-  database: 'Bamazon'
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'Bamazon'
 
 });
 
 //Connect to the database
-connection.connect(function(err){
-  if(err) {
-    throw err
-  }
-  else
-  {
-
-      displayAllProducts();
-      //connection.end();
-}
+connection.connect(function(err) {
+    if (err) throw err
+    displayAllProducts();
+    //connection.end();
 })
 
 //Display all Products in inventory
 var displayAllProducts = function() {
 
-  connection.query(query.sqlQuery.selectAllProducts,function(selectQueryError,results,fields){
-    if (selectQueryError) throw selectQueryError;
-    var table = new Table({
-      head: tableDisplayDefinition.headers.fullTable.fullTableColumns,
-      colWidths: tableDisplayDefinition.headers.fullTable.fullTableWidth
-  });
-    results.forEach(function(row,index){
-      var displayArray = new Array(5);
-      displayArray[0] = row.item_id,
-      displayArray[1] = row.product_name,
-      displayArray[2] = row.department_name,
-      displayArray[3] = row.price,
-      displayArray[4] = row.stock_quantity
-      table.push(displayArray);
+    connection.query(query.sqlQuery.selectAllProducts, function(selectQueryError, results, fields) {
+        if (selectQueryError) throw selectQueryError;
+        var table = new Table({
+            head: tableDisplayDefinition.headers.fullTable.fullTableColumns,
+            colWidths: tableDisplayDefinition.headers.fullTable.fullTableWidth
+        });
+        results.forEach(function(row, index) {
+            var displayArray = new Array(5);
+            displayArray[0] = row.item_id,
+                displayArray[1] = row.product_name,
+                displayArray[2] = row.department_name,
+                displayArray[3] = row.price,
+                displayArray[4] = row.stock_quantity
+            table.push(displayArray);
+        })
+        console.log(table.toString());
+        requestForPurchase();
     })
-    console.log(table.toString());
-    requestForPurchase();
-  })
 
 }
 
 var requestForPurchase = function() {
-  inquirer.prompt(questions.prompt.inquirePurchaseQuestion).then(function(userResponse) {
-    if(userResponse.inquirePurchaseAnswer.toLowerCase() == 'yes' || userResponse.inquirePurchaseAnswer.toLowerCase() == 'y')
-    {
-      purchaseProducts();
-    }
-
-  })
+    inquirer.prompt(questions.prompt.inquirePurchaseQuestion).then(function(userResponse) {
+        if (userResponse.inquirePurchaseAnswer.toLowerCase() == 'yes' || userResponse.inquirePurchaseAnswer.toLowerCase() == 'y') {
+            purchaseProducts();
+        } else {
+            connection.end();
+        }
+    })
 
 }
 
-var purchaseProducts = function() {
-  inquirer.prompt(questions.prompt.purchaseProductQuestion).then(function(itemAnswer) {
-    inquirer.prompt(questions.prompt.purchaseQuantityQuestion).then(function(quantityAnswer) {
-        connection.query(query.sqlQuery.searchProductByName,"%"+ [itemAnswer.requestedItem] + "%",function(selectQueryError,results,fields){
-        if (selectQueryError) throw selectQueryError;
-        var dbStockQuantity = 0;
-        var dbItemId = 0
-        results.forEach(function(row,index){
-            dbStockQuantity = row.stock_quantity;
-            dbItemId = row.item_id;
-        })
-        if(dbStockQuantity >= quantityAnswer.requestedNumber) {
+var printReceipt = function(productName,productPrice,productQuantity,productId) {
+console.log('-----------------------------------------------------------');
+console.log('               BAMAZON   RECEIPT                    ');
+console.log('         eat sleep code repeat forever                     ');
+console.log('ST# 2006 OP# 0006176  TE# 03                       ');
+console.log(productName +'                         '+ productPrice + ' * ' + productQuantity  + '        ' + (productPrice*productQuantity));
+console.log(new Date());
+console.log('          # ITEMS SOLD 1         ');
+console.log('-----------------------------------------------------------');
+}
 
-          connection.query(query.sqlQuery.purchaseItem,[dbStockQuantity - quantityAnswer.requestedNumber,dbItemId],function(selectQueryError,results,fields){
-            console.log("Items Successfully Purchased");
-            displayAllProducts();
-          })
-        }
-        
-      })
+var purchaseProducts = function() {
+    inquirer.prompt(questions.prompt.purchaseProductQuestion).then(function(itemAnswer) {
+        inquirer.prompt(questions.prompt.purchaseQuantityQuestion).then(function(quantityAnswer) {
+            connection.query(query.sqlQuery.searchProductByName, "%" + [itemAnswer.requestedItem] + "%", function(selectQueryError, results, fields) {
+                if (selectQueryError) throw selectQueryError;
+                console.log(results.length);
+                if(results.length === 1)
+                {
+                  var dbStockQuantity = 0;
+                  var dbItemId = 0;
+                  var dbPrice = 0;
+                  var dbProductName = '';
+                  results.forEach(function(row, index) {
+                      dbStockQuantity = row.stock_quantity;
+                      dbItemId = row.item_id;
+                      dbPrice = row.price;
+                      dbProductName = row.product_name;
+                  })
+                  if (dbStockQuantity >= quantityAnswer.requestedNumber) {
+                          //Update Products Inventory
+                          connection.query(query.sqlQuery.purchaseItem, [dbStockQuantity - quantityAnswer.requestedNumber, dbItemId], function(selectQueryError, results, fields) {
+                          console.log("Items Successfully Purchased");
+                          printReceipt(dbProductName,dbPrice,quantityAnswer.requestedNumber,dbItemId);
+                          displayAllProducts();
+                      })
+                  } else {
+                      console.log(colors.red('\u2717' + ' Insufficient Quantity - Please enter a valid quantity'));
+                      requestForPurchase();
+                  }
+
+                } else if (results.length === 0)
+
+                {
+                  console.log(colors.red('\u2717' + ' No results matched your criteria - Please enter a valid Product name'));
+                  requestForPurchase();
+                }
+                else {
+                  console.log(colors.red('\u2717' + ' More than one results matched your criteria - Please enter a Product'));
+                  requestForPurchase();
+                }
+
+            })
+
+        })
 
     })
-
-  })
 }
